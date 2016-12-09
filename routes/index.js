14d5963770bf4fb746f4b89d31eab5d859721534
iteming <coolon@126.com>
2016-12-09 18:04:46 +0800

    update demo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     // 跳转到index页面,同时传递 option 为 {title: 'Express'}
-    res.render('index', {title: 'Express', name: 'tj', email: 'tj@vision-media.ca' });
+    res.render('index', {title: 'Express', user: {id: 12, name: 'tj'}, email: 'tj@vision-media.ca'});
     // 直接向页面发送信息
     // res.send('respond with a resource');
 });
