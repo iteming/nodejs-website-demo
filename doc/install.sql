768feb981baca2f686d07fa327ee65c9087e3d56
jiazhaokai <jiazhaokai@gsoft.cc>
2016-12-10 22:04:29 +0800

    database design

--- doc/install.sql
+++ doc/install.sql
@@ -2,14 +2,198 @@
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `cms_account`
+-- Table structure for `user`
 -- ----------------------------
-DROP TABLE IF EXISTS `cms_account`;
-CREATE TABLE `cms_account` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `displayname` varchar(100) DEFAULT NULL,
+  `status` int(1) DEFAULT 1,
+  `createtime` datetime DEFAULT NULL,
+  `lastlogintime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `company`
+-- ----------------------------
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `company_type` varchar(100) DEFAULT NULL,
+  `company_size` varchar(100) DEFAULT NULL,
+  `regist_capital` varchar(100) DEFAULT NULL,
+  `regist_date` datetime DEFAULT NULL,
+  `business_model` varchar(100) DEFAULT NULL,
+  `business_scope` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `contact`
+-- ----------------------------
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_name` varchar(100) DEFAULT NULL,
+  `contact_phone` varchar(100) DEFAULT NULL,
+  `contact_email` varchar(100) DEFAULT NULL,
+  `contact_mobile` varchar(100) DEFAULT NULL,
+  `contact_fax` varchar(100) DEFAULT NULL,
+  `contact_qq` varchar(100) DEFAULT NULL,
+  `location_lng` varchar(100) DEFAULT NULL,
+  `location_lat` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(1) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL,
+  `comment` varchar(500) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `contact` varchar(100) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `parentid` int(11) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `status` int(1) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `notice`
+-- ----------------------------
+DROP TABLE IF EXISTS `notice`;
+CREATE TABLE `notice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `status` int(1) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `news`
+-- ----------------------------
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `status` int(1) DEFAULT 1,
+  `views` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `category`
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
-  `account` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `product`
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `model` varchar(100) DEFAULT NULL,
+  `specification` varchar(100) DEFAULT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `price` varchar(100) DEFAULT NULL,
+  `detail_info` text DEFAULT NULL,
+  `publish_date` datetime DEFAULT NULL,
+  `expiry_date` datetime DEFAULT NULL,
+  `views` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `picture`
+-- ----------------------------
+DROP TABLE IF EXISTS `picture`;
+CREATE TABLE `picture` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pic_type` int(1) DEFAULT NULL,
+  `key_id` int(11) DEFAULT NULL,
+  `pic_name` varchar(100) DEFAULT NULL,
+  `pic_url_cdn` varchar(100) DEFAULT NULL,
+  `pic_url_loc` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `honor`
+-- ----------------------------
+DROP TABLE IF EXISTS `honor`;
+CREATE TABLE `honor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `honor_name` varchar(100) DEFAULT NULL,
+  `honor_main_id` int(11) DEFAULT NULL,
+  `certification` varchar(100) DEFAULT NULL,
+  `publish_date` datetime DEFAULT NULL,
+  `expiry_date` datetime DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `views` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `photo`
+-- ----------------------------
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE `photo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `photo_name` varchar(100) DEFAULT NULL,
+  `photo_main_id` int(11) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `views` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for `website`
+-- ----------------------------
+DROP TABLE IF EXISTS `website`;
+CREATE TABLE `website` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `siteurl` varchar(100) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `logo` varchar(100) DEFAULT NULL,
+  `carousel` varchar(500) DEFAULT NULL,
+  `icp_num` varchar(100) DEFAULT NULL,
+  `support_name` varchar(100) DEFAULT NULL,
+  `support_url` varchar(100) DEFAULT NULL,
+  `views` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
