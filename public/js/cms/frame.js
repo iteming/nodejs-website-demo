943633cb1d806c33c66033cdda2afe40587c0180
iteming <coolon@126.com>
2016-12-12 17:56:53 +0800

    cms layout and index

---
+++ public/js/cms/frame.js
@@ -0,0 +1,109 @@
+// JavaScript Document
+$(function () {
+    $(".wt_dd").hover(function () {
+        $(this).find(".drop_down").show();
+    }, function () {
+        $(this).find(".drop_down").hide();
+    })
+
+
+    $(".sider_bar .bar_top .search_bar").focus(function () {
+        $(this).css("background-color", "white");
+    })
+    $(".sider_bar .bar_top .search_bar").blur(function () {
+        $(this).css("background-color", "#374850");
+    })
+
+    /*$(".menu_parent").click(function(){
+         $(this).siblings(".sub_menu").slideDown(400);
+         $(this).parent().siblings().children(".sub_menu").slideUp(400);
+         $(this).find(".arrow_down").css("background-position","0 -16px");
+         $(this).parent().siblings().find(".arrow_down").css("background-position","0 0");
+         $(this).addClass("cur");
+         $(this).parent().siblings().children(".menu_parent").removeClass("cur");
+     })*/
+
+    $(".menu_parent").click(function () {
+        $(this).addClass("cur");
+        $(this).parent().siblings().children(".menu_parent").removeClass("cur");
+
+        $(this).parent().siblings().children(".sub_menu").slideUp(400);
+        $(this).parent().siblings().find(".arrow_down").css("background-position", "0 0");
+
+        var ul = $(this).siblings(".sub_menu");
+
+        if (ul.css('display') == 'none') {
+            $(this).siblings(".sub_menu").slideDown(400);
+            $(this).find(".arrow_down").css("background-position", "0 -16px");
+        } else {
+            $(this).siblings(".sub_menu").slideUp(400);
+            $(this).find(".arrow_down").css("background-position", "0 0px");
+        }
+    })
+
+
+    /*$(".sub_menu li").click(function(){
+         $(this).addClass("cur");
+         $(this).siblings("li").removeClass("cur")
+     })*/
+
+    // 三级菜单
+    $(".sub_menu li a").click(function () {
+        var parentli = $(this).parent("li");
+        parentli.addClass("cur");
+        parentli.siblings("li").removeClass("cur");
+
+        parentli.siblings("li").children(".sub_sub_menu").slideUp(400);
+        parentli.siblings("li").find(".arrow_down").css("background-position", "0 0");
+
+        var sub_ul = parentli.children(".sub_sub_menu");
+
+        if (sub_ul.css('display') == 'none') {
+            sub_ul.slideDown(400);
+            sub_ul.parent("li").find(".arrow_down").css("background-position", "0 -16px");
+
+        } else {
+            sub_ul.slideUp(400);
+            sub_ul.parent("li").find(".arrow_down").css("background-position", "0 0px");
+        }
+    })
+
+    $(".sub_sub_menu li").click(function () {
+        $(this).addClass("cur");
+        $(this).siblings("li").removeClass("cur");
+    })
+})
+
+$(function () {
+    var index_x = $(".page_index h5").html();
+    var index_y = $(".page_index p").html();
+    $(".menu_single").eq(index_x).find(".menu_parent").addClass("cur");
+    $(".menu_single").eq(index_x).find(".sub_menu").find("li").eq(index_y).addClass("cur")
+})
+
+$(function () {
+    $(".menu_ctl").click(function () {
+        if ($(".sider_bar").css("left") == "0px") {
+            $(".sider_bar").css("left", "-250px")
+            $(".content_main").css("left", "0px")
+            $(this).find("img").css("margin-top", "-0px")
+        }
+        else {
+            $(".sider_bar").css("left", "0px")
+            $(".content_main").css("left", "250px");
+            $(this).find("img").css("margin-top", "-50px")
+        }
+    })
+})
+
+$(function () {
+    $(".welcome").show();
+    $(".welcome").animate({"top": "50%"}, 500);
+    $(".welcome").css("margin-top", "-180px");
+
+    $(".start_using").click(function () {
+        $(".shelter").fadeOut(300);
+        $(".welcome").fadeOut(300)
+    })
+
+})
--- routes/cms/views.js
+++ routes/cms/views.js
@@ -5,13 +5,34 @@ require('../core/HttpWrapper');
 require('../core/SqlClient');
 require('../model/model');
 
+
 router.get('/', function (req, res, next) {
-    res.render('cms/index');
+    res.redirect('index');
+});
+
+router.get('/index', function (req, res, next) {
+    var user = req.cookies.user;
+    console.log('index start ↓');
+    console.log(user);
+    if(user){
+        res.render('cms/index', {user: user});
+    }else{
+        res.redirect('login');
+    }
 });
 
 router.get('/login', function (req, res, next) {
     res.render('cms/login',{username:''});
 });
+
+router.get('/logout', function (req, res, next) {
+    console.log('logout start ↓');
+    //清空user信息
+    console.log(req.cookies);
+    res.cookie('user', null, {maxAge: 0})
+    res.redirect('login');
+});
+
 
 router.post('/login', function (req, res, next) {
     console.log(req.body.username);
@@ -27,6 +48,8 @@ router.post('/login', function (req, res, next) {
             if(user.password != req.body.password){
                 res.render('cms/login', {status: 2,msg: '密码错误!', username:req.body.username});
             }
+            // res.cookie('user', user, {maxAge: 24 * 60 * 60 * 1000});
+            res.cookie('user', user, {maxAge: 60 * 1000});
             res.render('cms/index', {status: 1,msg: '登录成功!', user: user});
         }
         res.render('cms/login', {status: 3,msg: '帐号不存在!', username:req.body.username});
