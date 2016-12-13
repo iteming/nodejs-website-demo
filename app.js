e17a06d4627271469d6887558c8384e606529017
jiazhaokai <jiazhaokai@gsoft.cc>
2016-12-13 21:47:35 +0800

    update menu icon and cms font-size

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'jade');
 // app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
